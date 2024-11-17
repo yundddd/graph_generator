b62707614c28b41648445a1f479c9988de4e28c1
Tim Tianyang Chen <soapcn@gmail.com>
2024-11-16 21:34:17 -0800

    [notebook] port gnn notebook to run locally

--- tools/jupyter/main.py
+++ tools/jupyter/main.py
@@ -8,4 +8,6 @@ if __name__ == "__main__":
     if "BUILD_WORKSPACE_DIRECTORY" in os.environ:
         os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"])
     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
+    sys.argv.append("--NotebookApp.token=\'\'")
+    sys.argv.append("--no-browser")
     sys.exit(main())
