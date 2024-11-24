a6cb84aba2f01cc6a87c1a0a12f1a23029711a0e
tchen <4584143+yundddd@users.noreply.github.com>
2024-11-23 22:37:05 -0800

    [notebook] port gnn notebook to run locally (#19)
    * [notebook] port gnn notebook to run locally
    
    * Add windows requirements
    
    * fix platform lock file
    
    * fix setuptool for linux
    
    * update for mac

--- tools/jupyter/main.py
+++ tools/jupyter/main.py
@@ -2,10 +2,12 @@ import os
 import re
 import sys
 
-from notebook.notebookapp import main
+from notebook.app import main
 
 if __name__ == "__main__":
     if "BUILD_WORKSPACE_DIRECTORY" in os.environ:
         os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"])
     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
+    sys.argv.append("--NotebookApp.token=\'\'")
+    sys.argv.append("--no-browser")
     sys.exit(main())
